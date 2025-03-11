7d99c2dc86f881ab1570034848aae4ec48881195
Jesus Barrientos <jesus.barrientosg.95@gmail.com>
2025-03-11 08:51:53 -0300

    fix rebase

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,6 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { MapController } from './components';
 
 function App() {
-  const source = [
-    {
-      id: 'points',
-      data: {
-        type: 'geojson',
-        generateId: true,
-        data: {
-          type: 'FeatureCollection',
-          features: [
-            {
-              type: 'Feature',
-              geometry: {
-                type: 'Point',
-                coordinates: [-74.5, 40]
-              },
-              properties: {
-                name: 'The Gilded Fork',
-                category: 'entertainment',
-                subCategory: 'restaurant'
-              }
-            },
-            {
-              type: 'Feature',
-              geometry: {
-                type: 'Point',
-                coordinates: [-75, 40]
-              },
-              properties: {
-                name: 'The Starlight Cinema',
-                category: 'entertainment',
-                subCategory: 'cinema'
-              }
-            }
-          ]
-        }
-      }
-    }
-  ]
-
   return (
     <MapController />
   )
